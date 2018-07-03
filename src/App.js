94d3cefc099ac9e69023902db55f77069a0f9e84
RiCK BROWN <ricky.brown.00@gmail.com>
2018-07-03 22:43:03 +0100

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import NavBar from './components/NavBar'
+import SearchBar from './components/SearchBar'
+import SearchResults from './components/SearchResults'
+import Footer from './components/Footer'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <NavBar />
+        <SearchBar />
+        <SearchResults />
+        <Footer />
       </div>
     )
   }
---
+++ src/components/SearchResults.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Searchresults = () => {
+  return (
+    <h1>SearchResults</h1>
+  );
+};
+
+export default Searchresults;
