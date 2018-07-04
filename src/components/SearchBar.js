52dbe5304238455088eded797a061838d42b4724
RiCK BROWN <ricky.brown.00@gmail.com>
2018-07-04 20:44:36 +0100

    Test

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const SearchBar = props => (
   <div className="search-bar u-margin-bottom-medium">
-    <form onSubmit={props.getRecipes}>
-      <input className="search-input" placeholder="Enter recipe name or ingredients" name="ingredients" />
+    <form onSubmit={props.getRecipes} autoComplete="off">
+      <input className="search-input" placeholder="Enter recipe name or ingredients" name="ingredients" id="search" />
+      <label htmlFor="search" className="search-label">Search for image</label>
       <button className="search-button">Search</button>
     </form>
   </div>
